1d5b9f211819163aa16f8f153be50944b01370c2
Anton <ushkov6@mail.ru>
2021-10-26 17:40:42 +0400

    fetch response func part2

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
-
-
 class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=73da627187167e6c6216bd7cbd93f7f2';
+
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -10,4 +11,16 @@ class MarvelService {
 
         return await res.json();
     }
+
+
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacterById = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
 }
+
+export default MarvelService;
