4df5b1b370a2b09df5dc7105f372d47be29b0476
Anton <ushkov6@mail.ru>
2021-10-26 17:11:46 +0400

    fetch response func

---
+++ src/services/MarvelService.js
@@ -0,0 +1,13 @@
+
+
+class MarvelService {
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+}
