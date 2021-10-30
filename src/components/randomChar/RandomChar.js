0825c1f942ed4c5ef13bbad1668dee501aa10b6c
Anton <ushkov6@mail.ru>
2021-10-30 13:03:18 +0400

    dynamic loading random character

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -6,10 +6,6 @@ import mjolnir from '../../resources/img/mjolnir.png';
 import MarvelService from '../../services/MarvelService';
 
 class RandomChar extends Component {
-    constructor(props) {
-        super(props);
-        this.updateChar();
-    }
 
     state = {
         char: {},
@@ -18,6 +14,15 @@ class RandomChar extends Component {
     }
 
     marvelService = new MarvelService();
+
+    componentDidMount() {
+        this.updateChar();
+        // this.timerId = setInterval(this.updateChar, 3000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
 
     onCharLoaded = (char) => {
         this.setState({
