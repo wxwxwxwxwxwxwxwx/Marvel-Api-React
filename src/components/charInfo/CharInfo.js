4059f447c0ffb1d4ee0a43a90ccade05741015e2
Anton <ushkov6@mail.ru>
2021-11-01 12:34:54 +0400

    prop-types

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import {Component} from 'react';
+import PropTypes from 'prop-types'
 import './charInfo.scss';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -125,6 +126,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
