0ec8169672552b5e09f27c73897a0da6f8d1e59b
Anton <ushkov6@mail.ru>
2021-11-17 17:27:57 +0400

    fix error

--- src/hooks/http.hook.js
+++ src/hooks/http.hook.js
@@ -11,6 +11,10 @@ export const useHttp = () => {
         try {
             const response = await fetch(url, {method, body, headers});
 
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+            }
+
             const data = await response.json();
 
             setLoading(false);
