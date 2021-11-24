204b3c249d8793130a26306266176a2bc9cd0083
Anton <ushkov6@mail.ru>
2021-11-24 17:58:32 +0400

    git meta tags and titles on pages

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet';
+
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
@@ -6,6 +8,13 @@ const ComicsPage = () => {
     
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with list of comics"
+                    />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner/>
             <ErrorBoundary>
                 <ComicsList/>
