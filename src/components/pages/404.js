af053335dd1d53e0f526edba307ee014b60ccb08
Anton <ushkov6@mail.ru>
2021-11-24 22:11:44 +0400

    git meta tags and titles on pages v2

--- src/components/pages/404.js
+++ src/components/pages/404.js
@@ -1,10 +1,22 @@
-import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Helmet } from "react-helmet";
 import {Link} from 'react-router-dom';
+
+import ErrorMessage from "../errorMessage/ErrorMessage";
+
 const Page404 = () => {
     return (
         <div>
-            <ErrorMessage/>
-            <Link to="/">Back to main page</Link>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="This page is not found"
+                />
+                <title>This page is not found</title>
+            </Helmet>
+            <div>
+                <ErrorMessage/>
+                <Link to="/">Back to main page</Link>
+            </div>
         </div>
     )
 }
